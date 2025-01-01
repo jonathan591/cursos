1fba93993673f9344f8d9a675daab37f323ea02f
jonathan cedeño <77215819+jonathan591@users.noreply.github.com>
2025-01-01 08:13:59 -0500

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,9 +4,4 @@ composer global remove hirak/prestissimo
 composer clear-cache
 composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
 
-echo "Caching config..."
-php artisan config:cache
-echo "Caching routes..."
-php artisan route:cache
-echo "Running migrations..."
-php artisan migrate
+
